1b1725c1704a21d0c7d93f3726eca54f5988fe65
libakoer <arturtamm02@gmail.com>
2026-02-09 12:42:49 +0200

    Added the Script file for future, and fixed the spelling mistake.

--- menus/wifi_menu.py
+++ menus/wifi_menu.py
@@ -5,7 +5,7 @@ from textual.app import ComposeResult
 class WifiMenu(VerticalScroll):
     def compose(self) -> ComposeResult:
         yield Button("Wifi system credentials setup - system.conf file", id="wifi_conf")
-        yield Button("Wifi network initial setup - OpenWRT rouyter", id="openwrt")
+        yield Button("Wifi network initial setup - OpenWRT router", id="openwrt")
         yield Button("Update Wifi Credentials on OpenWRT router")
         yield Button("Set-up wifi gateway on the Raspberry Pi")
         yield Button("Back", id="back")
